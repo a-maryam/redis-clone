f91db60f3ff70851b9162904b324ea1ac2982c00
Aleena Maryam <aleenamaryam@gmail.com>
2025-12-22 16:50:15 -0500

    status commit

--- include/struct.h
+++ include/struct.h
@@ -4,14 +4,26 @@ Purpose: so far hold structs used at the heart of the key-value store
 
 // key will be char[] 
 
+typedef enum {
+    VALUE_FLOAT;
+    VALUE_CHAR;
+    VALUE_DOUBLE;
+    VALUE_INT;
+    VALUE_INT_PTR;
+    VALUE_CHAR_PTR;
+} ValueType;
+
 // union must hold pointers.
-union Value {
-    float f;
-    char c; 
-    double d;
-    int i;
-    int *arr_ptr; // will be dynamically allocated and live on heap
-    int *char_ptr;
-    // create structs for other data types later
-    // lists, heaps, trees...
-}
+typedef struct {
+    enum ValueType value;
+    union {
+        float f;
+        char c; 
+        double d;
+        int i;
+        int *arr_ptr; // will be dynamically allocated and live on heap
+        int *char_ptr;
+        // create structs for other data types later
+        // lists, heaps, trees...
+    } Value
+} Value
