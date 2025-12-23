30f143e0647b8c5f153a902059d2ab9b81023db5
Aleena Maryam <aleenamaryam@gmail.com>
2025-12-23 12:41:50 -0500

    tokenizing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "../include/struct.h"
 // probably have to change from relative to whatever best practice is
 
-int read() {
+int* read() {
     char line[100];
     printf("**Toy Redis Started**\n Enter Commands:\n");
     // will need to implement error handling for invalid inputs
@@ -12,10 +14,54 @@ int read() {
     else {
         printf("Error reading input.");
     }
-    return 0;
+    return line;
+} // need to free...parse will free
+
+int parse(char[] input) {
+    // strtok should go till null character in line, right?
+    // forget what ur supposed to do when you have a variable number 
+    // of input tokens lol
+    int tokens_allowed = 4;
+    const char delimiters[] = " "; // might want to accommodate extra whitespace in future
+    char *token;
+    char *tokens_arr[tokens_allowed]; // gonna need checks, expect only 3 tokens atm
+    token = strtok(input, delimiters);
+
+    int i = 0;
+    
+    while(token!=NULL && i < tokens_allowed) {
+        tokens_arr[i++] = token; // store ptr
+        token = strtok(NULL, delimiters); // get next token
+    }
+
+    printf("Tokens\n");
+
+    for(int j = 0; j < i; j++) {
+        printf("Token %d: %s\n", j+1, tokens_arr[j]);
+    }
+    // need tokens, and input if passing along. 
+    // can just copy to new vars and pass. easier
+
+    if(strcmp(tokens_arr[0], "GET")==0) {
+        
+        get()
+    }
+    return 0; // need my hash table...before i can write the functions
+}
+
+set() {
+
+}
+
+get() {
+
+}
+
+del() {
+
 }
 
 int main(void) {
-    read();
+    parse(read());
     return 0;
 }
