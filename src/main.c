e43259bab32f4e8b2b4aa6ef9fafd59e36bc27cd
Aleena Maryam <aleenamaryam@gmail.com>
2025-12-23 11:53:13 -0500

    started io

--- src/main.c
+++ src/main.c
@@ -2,6 +2,20 @@
 #include "../include/struct.h"
 // probably have to change from relative to whatever best practice is
 
-main() {
-    printf("\ntest\n");
+int read() {
+    char line[100];
+    printf("**Toy Redis Started**\n Enter Commands:\n");
+    // will need to implement error handling for invalid inputs
+    if(fgets(line, sizeof(line), stdin) != NULL) {
+        printf("Entered: %s", line);
+    } 
+    else {
+        printf("Error reading input.");
+    }
+    return 0;
+}
+
+int main(void) {
+    read();
+    return 0;
 }
